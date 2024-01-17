720eac9d14b722e7b0d7ad78c8a25fad9fc8b190
Accelerator321 <shyamprime2610@gmail.com>
2024-01-17 17:52:08 +0530

    env varaibles chnaged

--- frontend/src/contexts/Url.jsx
+++ frontend/src/contexts/Url.jsx
@@ -1,5 +1,7 @@
-
+import dotenv from 'dotenv';
+dotenv.config(); 
 export default function useUrl(path){
-    const base = "http://localhost"
+    const base = process.env.REACT_APP_API_URL;
+    // console.log(base);
     return (base + path);
 }

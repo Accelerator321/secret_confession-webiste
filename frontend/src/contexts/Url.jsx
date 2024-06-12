672f8c458996366273256b23020a4a0dc65c7efb
Accelerator321 <shyamprime2610@gmail.com>
2024-06-12 13:54:44 +0530

    package.json update

--- frontend/src/contexts/Url.jsx
+++ frontend/src/contexts/Url.jsx
@@ -1,7 +1,8 @@
 // import dotenv from 'dotenv';
 // dotenv.config(); 
 export default function useUrl(path){
-    const base =import.meta.env.VITE_APP_API_URL;
+    // const base =import.meta.env.VITE_APP_API_URL;
+    const base= "https://secret-confession-backend.vercel.app/";
     // console.log(base);
     return (base + path);
 }
